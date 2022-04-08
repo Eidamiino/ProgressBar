110ffc41e27f0586defc742b33700301183bec73
Eidamiino <adam.pala199@gmail.com>
2022-04-08 23:12:29 +0200

    made the whole code

--- ProgressBar/ProgressBar/Program.cs
+++ ProgressBar/ProgressBar/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading;
 
@@ -8,17 +9,18 @@ namespace ProgressBar
 {
 	internal class Program
 	{
-		public const int steps = 40;
+		public const int steps = 50;
 		static void Main(string[] args)
 		{
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.CursorVisible = false;
 			//ProgBar1stType();
-			ProgBar2ndType();
+			//ProgBar2ndType();
+			//RandBar1stType();
 		}
 
 		private static void ProgBar1stType()
 		{
-			Console.OutputEncoding = Encoding.UTF8;
-			Console.CursorVisible = false;
 			for (int i = 1; i <= steps; i++)
 			{
 				for (int j=0; j < i; j++)
@@ -33,7 +35,42 @@ namespace ProgressBar
 
 		private static void ProgBar2ndType()
 		{
+			for (int i = 0; i < steps; i++)
+			{
+				Console.Write($"\u2591");
+			}
 
+			for (int i = 0; i < steps; i++)
+			{
+				Console.CursorLeft = i;
+				for (int j = 0; j < 2; j++)
+				{
+					if(j==0)
+						Console.Write($"\u2593");
+					if(j==1&&i!=steps-1)
+						Console.Write($"\u2592");
+					Thread.Sleep(100);
+				}
+				Console.CursorLeft = steps + 1;
+				Console.Write($"{i+1}/{steps}");
+			}
+		}
+
+		private static void RandBar1stType()
+		{
+			Random rnd = new Random();
+			var randsteps = rnd.Next(25);
+			var randtime = rnd.Next(500);
+			for (int i = 1; i <= randsteps; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					Console.Write($"\u2593");
+				}
+				Console.Write($" {i}/{randsteps}");
+				Console.CursorLeft = 0;
+				Thread.Sleep(randtime);
+			}
 		}
 	}
 }
